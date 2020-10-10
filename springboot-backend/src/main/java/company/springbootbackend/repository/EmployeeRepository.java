07f6320b7fe63262cb5b585d6a478d515186c4be
SithuAung <gyrosithu007@gmail.com>
2020-10-10 15:16:48 +0630

    first commit

---
+++ springboot-backend/src/main/java/company/springbootbackend/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package company.springbootbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import company.springbootbackend.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
